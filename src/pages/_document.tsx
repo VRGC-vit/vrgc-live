9bb2b397765a1d9058674e769c11cd64c8fb9f10
RiShAv-MaNdAl3122 <rishavmandal376217@gmail.com>
2026-09-06 12:19:31 +0530

    feat(chat): add Tawk.to live chat script to _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,6 +29,24 @@ export default function Document() {
             rel="stylesheet"
           />
         </noscript>
+        {/* Start of Tawk.to Script */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6a9d07ec01ac02344ed3c52a/1k1qmdu5s';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
+        {/* End of Tawk.to Script */}
       </Head>
       <body>
         <Main />
